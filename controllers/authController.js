f6aa997c73c520da701221c41c417142510b3499
kenil <kenil.infynno@gmail.com>
2024-01-30 11:19:36 +0530

    forgot password api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 const Yup = require("yup");
 const dotenv = require('dotenv');
@@ -59,6 +60,10 @@ const registerUser = async (req, res) => {
 const signInUser = async (req, res) => {
     try {
         const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(400).json({success:false, message:"Please provide email and password to login"})
+        }
+
         const user = await User.findOne({
             where: {email}
         });
@@ -90,7 +95,124 @@ const signInUser = async (req, res) => {
     }
 }
 
+
+const changePassword = async (req, res) => {
+    try {
+        const { oldPassword, newPassword, confirmPassword } = req.body;
+        if(!oldPassword || !newPassword || !confirmPassword){
+            return res.status(400).json({success:false, message:"Please provide old password, new password and confirm password"})
+        }
+        else if (newPassword !== confirmPassword){
+            return res.status(400).json({success:false, message:"New password and confirm password does not match"})
+        }
+        const userId = req.user.id;
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+        const passwordValid = await bcrypt.compare(oldPassword, user.password);
+        if (!passwordValid) {
+            return res.status(401).json({ success: false, message: "Incorrect old password" });
+        }
+
+        const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedNewPassword;
+        await user.save();
+        return res.status(200).json({ success: true, message: "Password changed successfully" });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ success: false, message: "Error in changing password" });
+    }
+};
+
+const sendResetPasswordEmail = async(email, resetToken) => {
+    const transporter = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 587,
+        auth: {
+            user: process.env.user,
+            pass: process.env.password,
+        },
+    });
+    console.log(resetToken);
+
+    const mailOptions = {
+        from: 'test.infynno@gmail.com',
+        to: email,
+        subject: 'Reset Your Password',
+        html: `<p>Please click the following link to reset your password: <a href="http://localhost:3000/reset-password?token=${resetToken}">${resetToken}</a></p>`
+    };
+    console.log(mailOptions);
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Reset password email sent successfully');
+    } catch (error) {
+        console.error('Error sending reset password email:', error);
+    }
+}
+
+const forgotpassword = async (req, res) => {
+    try {
+        const { email } = req.body;
+        if(!email){
+            return res.status(400).json({ success: false, message: 'Please enter email to reset the password' });
+        }
+        console.log(email);
+        const user = await User.findOne({ email:email });
+        console.log({user});
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+        const resetToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        user.resetToken = resetToken;
+        await user.save();
+        await sendResetPasswordEmail(email, resetToken);
+        return res.status(200).json({ success: true, message: 'Reset token sent successfully' });
+    } catch (error) {
+        console.error('Error during forgot password:', error);
+        return res.status(500).json({ success: false, message: 'Invalid token' });
+    }
+}
+
+const resetPassword = async (req, res) => {
+    try {
+        const { token, newpassword, confirmpassword } = req.body;
+        if(!token || !newpassword || !confirmpassword) {
+            return res.status(400).json({success:false, message:"Please provide token, new password and confirm password fields"})
+        }
+        if (newpassword !== confirmpassword){
+            return res.status(404).json({success:false, message:"New password and confirm password is not same "})
+         }
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decodedToken.userId;
+        console.log(userId);
+
+        const user = await User.findOne(userId);
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+
+        const hashedNewPassword = await bcrypt.hash(newpassword, 10);
+        user.password = hashedNewPassword;
+        user.resetToken = null;
+        await user.save();
+
+        return res.status(200).json({ success: true, message: 'Password reset successfully' });
+    } catch (error) {
+        const errorMessage = error.message.replace(/\\|"/g, '');
+        if (errorMessage.includes('jwt expired')) {
+            return res.status(422).json({ success:false, message: `Your session has been expried`});
+        }
+        console.error('Error during password reset:', error);
+        return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
+}
+
 module.exports = {
     registerUser,
-    signInUser
+    signInUser,
+    changePassword,
+    forgotpassword,
+    resetPassword
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const { registerUser, signInUser} = require('../controllers/authController.js');
+const { registerUser, signInUser, changePassword, forgotpassword, resetPassword} = require('../controllers/authController.js');
+const verifyToken = require('../middleware/authMiddleware');
+
+
 const router = express.Router();
 
 router.post('/sign-up', registerUser);
 
 router.post('/sign-in', signInUser);
+
+router.post('/change-password', verifyToken, changePassword);
+
+router.post('/forgot-password', forgotpassword )
+
+router.post('/reset-password', resetPassword )
 
 module.exports = router;
